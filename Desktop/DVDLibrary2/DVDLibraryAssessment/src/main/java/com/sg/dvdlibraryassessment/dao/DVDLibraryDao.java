46fc11935c0c9ad7cc770f4af01dafc26a67bace
Teresa de Castro <decastro.ter@gmail.com>
2022-06-28 15:39:58 -0400

    Added set&get + DVDLibraryDao

--- Desktop/DVDLibrary2/DVDLibraryAssessment/src/main/java/com/sg/dvdlibraryassessment/dao/DVDLibraryDao.java
+++ Desktop/DVDLibrary2/DVDLibraryAssessment/src/main/java/com/sg/dvdlibraryassessment/dao/DVDLibraryDao.java
@@ -5,10 +5,40 @@
  */
 package com.sg.dvdlibraryassessment.dao;
 
+import com.sg.dvdlibraryassessment.dto.DVD;
+import java.util.List;
+
 /**
  *
  * @author Teresa
  */
 public interface DVDLibraryDao {
     
+    List<DVD> getAllDVDs();
+
+    DVD getDVD(String dvdID); // dvd info? -- refer to example on github
+    
+    DVD addDVD(String dvdID, DVD dvd);
+    
+    DVD removeDVD(String dvdID);
+    
+    DVD editDVD(String dvdID, DVD dvd, String prevDVDTitle);
+    
+    
 }
+// private String title;
+//    private String releaseDate;
+//    private String mpaaRating;
+//    private String directorsName;
+//    private String studioName;
+//    private String userRating;
+//    private String dvdID;
+//
+//io.print("Main Menu");
+//            io.print("1. Display DVD List"); //Display All DVDs using ID
+//            io.print("2. View DVD Information");
+//            io.print("3. Find DVDs");
+//            io.print("4. Add DVD");
+//            io.print("5. Remove DVD");
+//            io.print("6. Edit DVD");
+//            io.print("7. Exit");
--- Desktop/DVDLibrary2/DVDLibraryAssessment/src/main/java/com/sg/dvdlibraryassessment/dto/DVD.java
+++ Desktop/DVDLibrary2/DVDLibraryAssessment/src/main/java/com/sg/dvdlibraryassessment/dto/DVD.java
@@ -10,5 +10,66 @@ package com.sg.dvdlibraryassessment.dto;
  * @author Teresa
  */
 public class DVD {
+    private String title;
+    private String releaseDate;
+    private String mpaaRating;
+    private String directorsName;
+    private String studioName;
+    private String userRating;
+    private String dvdID;
     
+    public DVD(String dvdID){
+        this.dvdID = dvdID;
+    }
+    public String getDVDID() {
+        return dvdID;
+    }
+    
+    
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    
+    public String getReleaseDate(){
+        return releaseDate;
+    }
+    public void setReleaseDate(String releaseDate){
+        this.releaseDate = releaseDate;
+    }
+    
+    
+    public String getMpaaRating(){
+        return mpaaRating;
+    }
+    public void setMpaaRating(String mpaaRating){
+        this.mpaaRating = mpaaRating;
+    }
+    
+    
+    public String getDirectorsName(){
+        return directorsName;
+    }
+    public void setDirectorsName(String directorsName){
+        this.directorsName = directorsName;
+    }
+    
+    
+    public String getStudioName() {
+        return studioName;
+    }
+    public void setStudioName(String studioName){
+        this.studioName = studioName;
+    }
+    
+    
+    public String getUserRating() {
+        return userRating;
+    }
+    public void setUserRating(String userRating){
+        this.userRating = userRating;
+    }
 }
